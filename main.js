a06c0da276405f05ee7436567f2a641d40befe9f
Lavro Sandor <lavro.sandor@BOLYAI.LOCAL>
2024-09-27 15:46:27 +0200

    Make a table in Javascript

--- main.js
+++ main.js
@@ -19,3 +19,51 @@ const array = [
         lastname: 'Horváth'
     },
 ]
+const table=document.createElement(`table`);
+document.body.appendChild(table);
+
+const thead=document.createElement(`thead`);
+const theadraw= document.createElement(`tr`);
+const thlastname = document.createElement(`th`);
+const thfirstname = document.createElement(`th`);
+const tbody = document.createElement(`tbody`);
+
+
+for(const pers of array){
+    const tr = document.createElement('tr');
+    const td = document.createElement('td'); // lastname
+    const td2 = document.createElement('td'); //firstname1
+    const td3 = document.createElement('td');//firstname2
+ 
+ 
+ 
+    tbody.appendChild(tr);
+    tr.appendChild(td);
+    tr.appendChild(td2);
+    
+ 
+ 
+    td.innerHTML = pers.lastname;
+    td2.innerHTML = pers.firstname1;
+    td3.innerHTML = pers.firstname2;
+ 
+ if(pers.firstname2=== `undefined`){
+    td2.colspan = 2;
+ }
+ else{
+    tr.appendChild(td3);
+    
+ }
+ 
+}
+
+
+thfirstname.colSpan=2;
+thlastname.innerHTML=`Vezeteknev`;
+thfirstname.innerHTML=`  Keresztnev`;
+table.appendChild(thead);
+thead.appendChild(theadraw);
+theadraw.appendChild(thlastname);
+theadraw.appendChild(thfirstname);
+table.appendChild(tbody);
+
