83346e00408275f3b357dfb5e611aaf2b051cbea
Lavro Sandor <lavro.sandor@BOLYAI.LOCAL>
2024-10-04 15:12:03 +0200

    Extended array

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
-const array = [
+let array = [
     {
         firstname1: 'Géza',
         firstname2: 'Ferenc',
-        lastname: 'Kocsis'
+        lastname: 'Kocsis',
+        married: true,
+        pet: 'kutya'
     },
     {
         firstname1: 'Mária',
         firstname2: 'Júlia',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: false,
+        pet: 'macska'
     },
     {
         firstname1: 'Ferenc',
-        lastname: 'Balogh'
+        lastname: 'Balogh',
+        married: false,
+        pet: 'teknős'
     },
     {
         firstname1: 'Gábor',
         firstname2: 'Attila',
-        lastname: 'Horváth'
+        lastname: 'Horváth',
+        married: true,
+        pet: 'macska'
     },
 ]
 const table=document.createElement(`table`);
@@ -26,6 +34,8 @@ const thead=document.createElement(`thead`);
 const theadraw= document.createElement(`tr`);
 const thlastname = document.createElement(`th`);
 const thfirstname = document.createElement(`th`);
+const thmarried = document.createElement(`th`);
+const thpet = document.createElement(`th`);
 const tbody = document.createElement(`tbody`);
 
 
@@ -34,26 +44,51 @@ for(const pers of array){
     const td = document.createElement('td'); // lastname
     const td2 = document.createElement('td'); //firstname1
     const td3 = document.createElement('td');//firstname2
+    const td4 = document.createElement('td');//married
+    const td5 = document.createElement('td');//pet
  
- 
+    tr.addEventListener(`click`, function(e) {
+        console.log(`clicked`);
+        
+        const selector = tbody.querySelector(`.selected`);
+        
+        if(selector != undefined){
+            selector.classList.remove(`selected`)
+        }
+         e.currentTarget.classList.add(`selected`);
+    })
+   
  
     tbody.appendChild(tr);
     tr.appendChild(td);
     tr.appendChild(td2);
+   
     
  
  
     td.innerHTML = pers.lastname;
     td2.innerHTML = pers.firstname1;
     td3.innerHTML = pers.firstname2;
+    td4.innerHTML = pers.married
+    td5.innerHTML = pers.pet
+
+    if(pers.married === true){
+        td4.innerHTML=`igen`
+    }
+    else{
+        td4.innerHTML=`nem`
+    }
  
- if(pers.firstname2=== `undefined`){
-    td2.colspan = 2;
+ if(pers.firstname2=== undefined){
+    td2.colSpan = 2;
  }
  else{
     tr.appendChild(td3);
     
  }
+  tr.appendChild(td5);
+  tr.appendChild(td4);
+    
  
 }
 
@@ -61,9 +96,14 @@ for(const pers of array){
 thfirstname.colSpan=2;
 thlastname.innerHTML=`Vezeteknev`;
 thfirstname.innerHTML=`  Keresztnev`;
+thpet.innerHTML=`Háziállat`
+thmarried.innerHTML=`Házas`
+
 table.appendChild(thead);
 thead.appendChild(theadraw);
 theadraw.appendChild(thlastname);
 theadraw.appendChild(thfirstname);
+theadraw.appendChild(thpet)
+theadraw.appendChild(thmarried);
 table.appendChild(tbody);
 
